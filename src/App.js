a33185258ad487c00651fa78c15633d21c11e16a
craigscottuk <craigscottuk@gmail.com>
2022-06-18 19:27:08 +0200

    Added gameState to keep track of score + progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import './style.css';
+import { React, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routers, Route } from 'react-router-dom';
+import Error from './components/Error';
+import Loader from './components/Loader';
 
 export default function App() {
-  const [quizData, setQuizData] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [isError, setIsError] = React.useState(null);
+  const [quizData, setQuizData] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  function Game() {}
+  const [gameState, setGameState] = useState({
+    score: 0,
+    quesNum: 0,
+    isGameOver: false,
+  });
 
   // Fetching quiz data from opentdb.com/api
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchData();
   }, []);
 
   function fetchData() {
     fetch(
-      'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple'
+      'https://opentdb.com/api.php?amount=2&category=18&difficulty=easy&type=multiple'
     )
       .then((res) => {
         if (!res.ok) {
@@ -25,34 +33,43 @@ export default function App() {
       })
       .then((data) => {
         setIsLoading(false);
-        setIsError(null);
+        setIsError(false);
         setQuizData(data.results);
       })
       .catch((err) => {
         setIsLoading(false);
-        setIsError('Sorry! Unable to fetch the quiz data.');
+        setIsError(true);
       });
   }
 
-  // console.log(quizData);
-  quizData &&
-    quizData.forEach((quizQues, index) => {
-      const { question, correct_answer, incorrect_answers } = quizQues;
-      const allAnswers = shuffle([correct_answer, ...incorrect_answers]);
-      console.log(index);
-      console.log(question);
-      console.log(allAnswers);
-    });
+  // // console.log(quizData);
+  // quizData &&
+  //   quizData.forEach((quizQues, index) => {
+  //     const { question, correct_answer, incorrect_answers } = quizQues;
+  //     const allAnswers = shuffle([correct_answer, ...incorrect_answers]);
+  //     console.log(index);
+  //     console.log(question);
+  //     console.log(allAnswers);
+  //   });
 
-  // Shuffles the array of AllAnswers
-  function shuffle(allAnswersArr) {
-    return allAnswersArr.sort(() => Math.random() - 0.5);
-  }
+  // // Shuffles the array of AllAnswers
+  // function shuffle(allAnswersArr) {
+  //   return allAnswersArr.sort(() => Math.random() - 0.5);
+  // }
+
+  // console.log(quizData);
+
+  const { score, quesNum, isGameOver } = gameState;
+  const quizQues = quizData[quesNum];
 
   return (
     <div className='app'>
-      {isError && <div>{isError}</div>}
-      {isLoading && <div>Loading...</div>}
+      {isError && <Error error={isError} />}
+      {isLoading && <Loader isLoading={isLoading} />}
+      {quizData && <p>{score}</p>}
+      {quizData && <p>{quesNum}</p>}
+      {quizData && <p>{isGameOver ? 'Game is over' : 'Game is not over'}</p>}
+      {quizData && <p>{quizQues.question}</p>}
     </div>
   );
 }
