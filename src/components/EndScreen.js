21c68791806ed0a485cfaaa2c5c818856ccc69f6
craigscottuk <craigscottuk@gmail.com>
2022-06-21 21:36:43 +0200

    Created EndScreen component

---
+++ src/components/EndScreen.js
@@ -0,0 +1,12 @@
+/**
+ * EndScreen renders the final game state - the final score, a trophey and a retry button.
+ *
+ */
+
+import React from 'react';
+
+const EndScreen = () => {
+  return <div>EndScreen</div>;
+};
+
+export default EndScreen;
