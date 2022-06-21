02c83e1a10e5870f0e0d6809dd40bfe39b485169
craigscottuk <craigscottuk@gmail.com>
2022-06-21 21:36:28 +0200

    Created Answers component

---
+++ src/components/Answers.js
@@ -0,0 +1,12 @@
+/**
+ * Answers component renders the four answers to each quiz question.
+ *
+ */
+
+import React from 'react';
+
+const Answers = () => {
+  return <div className='answers-container'></div>;
+};
+
+export default Answers;
