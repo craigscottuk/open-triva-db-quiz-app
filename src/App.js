3b5059a8d1201578768b6a45999edf7f3cf22442
craigscottuk <craigscottuk@gmail.com>
2022-06-27 14:03:49 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Error from './components/Error';
 import Loader from './components/Loader';
 import QuizItem from './components/QuizItem';
 import Stats from './components/Stats';
+import useFetchData from './components/useFetchData';
 
 export default function App() {
-  const [quizData, setQuizData] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const { quizData, isLoading, isError } = useFetchData();
 
   function Game() {}
   const [gameState, setGameState] = useState({
@@ -17,50 +16,6 @@ export default function App() {
     quizDataIndex: 0,
     isGameOver: false,
   });
-
-  // Fetching quiz data from opentdb.com/api
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  function fetchData() {
-    fetch(
-      'https://opentdb.com/api.php?amount=2&category=18&difficulty=easy&type=multiple'
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw Error();
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setIsLoading(false);
-        setIsError(false);
-        setQuizData(data.results);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setIsError(true);
-      });
-  }
-
-  // // console.log(quizData);
-  // quizData &&
-  //   quizData.forEach((quizQuestion, index) => {
-  //     const { question, correct_answer, incorrect_answers } = quizQuestion;
-  //     const allAnswers = shuffle([correct_answer, ...incorrect_answers]);
-  //     console.log(index);
-  //     console.log(question);
-  //     console.log(allAnswers);
-  //   });
-
-  // // Shuffles the array of AllAnswers
-  // function shuffle(allAnswersArr) {
-  //   return allAnswersArr.sort(() => Math.random() - 0.5);
-  // }
-
-  // console.log(quizData);
 
   const { score, quizDataIndex, isGameOver } = gameState;
 
