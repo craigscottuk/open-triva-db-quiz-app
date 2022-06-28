49b27226a0e2d46c363cb690b9b3f8b052e3cead
craigscottuk <craigscottuk@gmail.com>
2022-06-28 21:24:54 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import { React, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routers, Route } from 'react-router-dom';
-import EndScreen from './components/EndScreen';
-import Error from './components/Error';
+import { React, useState } from 'react';
+// import { BrowserRouter as Router, Routers, Route } from 'react-router-dom';
+import useFetchQuizData from './components/useFetchQuizData';
 import Loader from './components/Loader';
-import QuizItem from './components/QuizItem';
+import Error from './components/Error';
 import Stats from './components/Stats';
-import useFetchData from './components/useFetchData';
+import QuizItem from './components/QuizItem';
+import EndScreen from './components/EndScreen';
+import { FadeWrapper, FadeTransition } from './components/fadeTransition';
 
-export default function App() {
-  const { quizData, isLoading, isError } = useFetchData();
+const App = () => {
+  // useFetchQuizData is a custom hook that fetches the quiz data
+  const { quizData, isLoading, isError } = useFetchQuizData(10, 'easy');
 
-  function Game() {}
   const [gameState, setGameState] = useState({
     score: 0,
     quizDataIndex: 0,
     isGameOver: false,
   });
-
-  const { score, quizDataIndex, isGameOver } = gameState;
-
-  const quizQuestion = quizData && quizData[quizDataIndex];
-  const { question, correct_answer, incorrect_answers } = quizQuestion;
-
-  const questionNum = quizDataIndex + 1;
-  const totalQuestions = quizData.length;
 
   // RestartGame function resets the gameState values thus restarting/resetting the game
   const restartGame = () => {
@@ -36,10 +29,30 @@ export default function App() {
 
   // LoadNextPage function uses spread operator to copy the gameState and adds 1 to quizDataIndex thus moving to the next page
   const loadNextPage = () => {
-    setGameState((prevGameState) => {
-      return { ...prevGameState, quizDataIndex: quizDataIndex + 1 };
-    });
+    if (quizDataIndex >= quizData.length - 1) {
+      setGameState({
+        ...gameState,
+        isGameOver: true,
+      });
+    } else {
+      setGameState({ ...gameState, quizDataIndex: quizDataIndex + 1 });
+    }
   };
+
+  const onAnswerSelected = (wasPlayerCorrect) => {
+    if (wasPlayerCorrect) {
+      setGameState((prevGameState) => {
+        return {
+          ...prevGameState,
+          score: score + 1,
+        };
+      });
+    }
+  };
+
+  const { score, quizDataIndex, isGameOver } = gameState;
+  const { question, correct_answer, incorrect_answers } =
+    quizData && quizData[quizDataIndex];
 
   return (
     <div className='app'>
@@ -51,26 +64,42 @@ export default function App() {
             <>
               <Stats
                 score={score}
-                questionNum={questionNum}
+                questionNum={quizDataIndex + 1}
                 isGameOver={isGameOver}
-                totalQuestions={totalQuestions}
+                totalQuestions={quizData.length}
               />
-              <QuizItem
-                question={question}
-                correct_answer={correct_answer}
-                incorrect_answers={incorrect_answers}
-                onNextBtnClick={loadNextPage}
-              />
+              <FadeWrapper>
+                <FadeTransition>
+                  <QuizItem
+                    key={quizDataIndex}
+                    question={question}
+                    correct_answer={correct_answer}
+                    incorrect_answers={incorrect_answers}
+                    onNextBtnClick={loadNextPage}
+                    onAnswerSelected={onAnswerSelected}
+                  />
+                </FadeTransition>
+              </FadeWrapper>
             </>
           ) : (
-            <EndScreen
-              score={score}
-              bestScore={0}
-              onRetryBtnClick={restartGame}
-            />
+            <>
+              {' '}
+              <FadeWrapper>
+                <FadeTransition>
+                  <EndScreen
+                    key={'EndScreen'}
+                    score={score}
+                    bestScore={0}
+                    onRetryBtnClick={restartGame}
+                  />
+                </FadeTransition>
+              </FadeWrapper>
+            </>
           )}
         </main>
       )}
     </div>
   );
-}
+};
+
+export default App;
--- src/components/QuizItem.js
+++ src/components/QuizItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 /**
  * QuizItem component renders each quiz question from the data.
@@ -6,7 +6,8 @@ import React from 'react';
  * @param {string} props.question
  * @param {string} props.correct_answer
  * @param {string} props.incorrect_answers
- * * @param {onClick} props.onNextBtnClick
+ * @param {()=> void} props.onNextBtnClick
+ * @param {(boolean)=> void)} props.onAnswerSelected
  */
 
 const QuizItem = ({
@@ -14,28 +15,77 @@ const QuizItem = ({
   correct_answer,
   incorrect_answers,
   onNextBtnClick,
+  onAnswerSelected,
 }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const hasPickedAnswer = selectedAnswer !== null;
+
   // Creates an array of allAnswers using correct_answer and expanding the incorrect_answers array.
-  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(
-    () => Math.random() - 0.5
-    //The array elements are shuffled using sort() and Math.
+  const allAnswers = [correct_answer, ...incorrect_answers];
+
+  // allAnswers are shuffled using sort() and Math.
+  const [shuffledAnswers] = useState(() =>
+    allAnswers.sort(() => Math.random() - 0.5)
   );
+
+  const selectAnswer = (event) => {
+    const playerAnswer = event.target.innerHTML;
+    setSelectedAnswer(playerAnswer);
+    const wasPlayerCorrect = playerAnswer === correct_answer;
+    onAnswerSelected(wasPlayerCorrect);
+  };
 
   return (
     <>
       <p>{question}</p>
-      <ul className='quizItem--answer-items-ul'>
-        {shuffledAnswers.map((answer, index) => {
-          return (
-            <li key={index} className='quizItem--answer-btn'>
-              {answer}
-            </li>
-          );
-        })}
-      </ul>
-
-      <button className='quizItem--next-btn' onClick={onNextBtnClick}>
-        Next
+      {selectedAnswer === correct_answer ? (
+        <div>
+          <button className='quizItem--answer-btn correct'>
+            {selectedAnswer}
+          </button>
+        </div>
+      ) : (
+        <div className='quizItem--answer-items-div'>
+          {shuffledAnswers &&
+            shuffledAnswers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={selectAnswer}
+                  className={
+                    !hasPickedAnswer
+                      ? 'quizItem--answer-btn'
+                      : answer === selectedAnswer && answer === correct_answer
+                      ? 'quizItem--answer-btn correct'
+                      : answer === selectedAnswer && answer !== correct_answer
+                      ? 'quizItem--answer-btn incorrect'
+                      : answer === correct_answer
+                      ? 'quizItem--answer-btn reveal-answer'
+                      : 'quizItem--answer-btn disabled'
+                  }
+                  disabled={hasPickedAnswer}
+                >
+                  {answer}
+                </button>
+              );
+            })}
+        </div>
+      )}
+      {
+        <button
+          className={
+            !hasPickedAnswer
+              ? 'quizItem--next-btn disabled'
+              : 'quizItem--next-btn'
+          }
+          onClick={onNextBtnClick}
+          disabled={!hasPickedAnswer}
+        >
+          Next
+        </button>
+      }
+      <button className='quizItem--quit-btn' onClick={onNextBtnClick}>
+        Quit
       </button>
     </>
   );
