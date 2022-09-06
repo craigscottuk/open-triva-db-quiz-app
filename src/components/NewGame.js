dc10345d76f8b465012f7dd76c644dfc2d3ffeb4
craigscottuk <craigscottuk@gmail.com>
2022-09-06 22:57:34 +0200

    Update

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, createTheme, ThemeProvider } from '@mui/material';
+import { amber } from '@mui/material/colors';
 
 const NewGame = () => {
+  const quizThemeMUI = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: amber,
+    },
+  });
+
   const navigate = useNavigate();
   return (
-    <div>
-      sauage
-      <button
-        onClick={() => {
-          navigate('/');
+    <ThemeProvider theme={quizThemeMUI}>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'grid',
+          justifyContent: 'center',
+          alignContent: 'center',
         }}
       >
-        New Game
-      </button>
-    </div>
+        <Button
+          onClick={() => {
+            navigate('/');
+          }}
+          variant='contained'
+          color='primary'
+          size='large'
+        >
+          New Game
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 };
 
